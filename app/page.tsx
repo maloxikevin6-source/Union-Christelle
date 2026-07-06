579e4af1a0c11ceb2ebc58356c1ebaa48c718677
maloxikevin6-source <maloxikevin6@gmail.com>
2026-07-06 15:07:09 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Bienvenue sur Union-Christelle</h1>
+      <p>Agence matrimoniale internationale.</p>
+      <p>Le site est actuellement en cours de développement.</p>
+    </main>
+  );
+}
